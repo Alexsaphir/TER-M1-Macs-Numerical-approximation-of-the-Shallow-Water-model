44d780e23a2c1fc24c128ecf2fc5fca3810d9665
Alexsaphir <alex.game@sfr.fr>
2018-03-30 17:34:32 +0200

    Rewrite CoupledGrid

--- Source/coupledgrid.cpp
+++ Source/coupledgrid.cpp
@@ -2,5 +2,53 @@
 
 CoupledGrid::CoupledGrid()
 {
+	m_grid1 = NULL;
+	m_grid2 = NULL;
 
+}
+
+CoupledGrid::~CoupledGrid()
+{
+
+}
+
+Grid* CoupledGrid::first() const
+{
+	return m_grid1;
+}
+
+Grid* CoupledGrid::second() const
+{
+	return m_grid2;
+}
+
+VectorR2 CoupledGrid::get(int i) const
+{
+	return VectorR2(getOnFirst(i), getOnSecond(i));
+}
+
+double CoupledGrid::getOnFirst(int i) const
+{
+	return m_grid1->get(i);
+}
+
+double CoupledGrid::getOnSecond(int i) const
+{
+	return m_grid2->get(i);
+}
+
+void CoupledGrid::set(int i, VectorR2 value)
+{
+	setOnFirst(i, value.x);
+	setOnSecond(i, value.y);
+}
+
+void CoupledGrid::setOnFirst(int i, double value)
+{
+	m_grid1->set(i, value);
+}
+
+void CoupledGrid::setOnSecond(int i, double value)
+{
+	m_grid2->set(i, value);
 }
--- Source/coupledgrid.h
+++ Source/coupledgrid.h
@@ -1,11 +1,30 @@
 #ifndef COUPLEDGRID_H
 #define COUPLEDGRID_H
 
+#include "grid.h"
+#include "vectorr2.h"
 
 class CoupledGrid
 {
 public:
 	CoupledGrid();
+	virtual ~CoupledGrid();
+
+	virtual Grid* first() const;
+	virtual Grid* second() const;
+
+	virtual VectorR2 get(int i) const;
+	double getOnFirst(int i) const;
+	double getOnSecond(int i) const;
+
+	virtual void set(int i, VectorR2 value);
+	void setOnFirst(int i, double value);
+	void setOnSecond(int i, double value);
+
+protected:
+	Grid *m_grid1;
+	Grid *m_grid2;
+
 };
 
 #endif // COUPLEDGRID_H
