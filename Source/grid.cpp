096630cb3b0e5bcc9e5b5bc1c185365638ee06d1
Alexsaphir <alex.game@sfr.fr>
2018-03-30 17:33:44 +0200

    Rewrite Grid

--- Source/grid.cpp
+++ Source/grid.cpp
@@ -1,6 +1,38 @@
 #include "grid.h"
 
-Grid::Grid()
+Grid::Grid(int N): m_n(N)
 {
+	m_V.reserve(m_n);
+	m_V.fill(0, m_n);
+}
 
+Grid::~Grid()
+{
+	m_V.clear();
+	m_V.squeeze();
+}
+
+int Grid::size() const
+{
+	return m_n;
+}
+
+double Grid::get(int i) const
+{
+	if(i<0 || i>=m_n)
+	{
+		qDebug() << "Set Grid error";
+		return 0.;
+	}
+	return m_V.at(i);
+}
+
+void Grid::set(int i, double v)
+{
+	if(i<0 || i>=m_n)
+	{
+		qDebug() << "Set Grid error";
+		return;
+	}
+	m_V.replace(i, v);
 }
--- Source/grid.h
+++ Source/grid.h
@@ -1,11 +1,25 @@
 #ifndef GRID_H
 #define GRID_H
 
+#include <QVector>
+
+#include <QDebug>
 
 class Grid
 {
 public:
-	Grid();
-};
+	Grid(int N);
+	virtual ~Grid();
 
+	virtual double get(int i) const;
+	virtual void set(int i, double v);
+
+	int size() const;
+
+	bool isValidIndex(int i) const;
+
+protected:
+	int m_n;
+	QVector<double> m_V;
+};
 #endif // GRID_H
