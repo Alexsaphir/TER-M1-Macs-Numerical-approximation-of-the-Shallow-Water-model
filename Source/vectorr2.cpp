ca1127fcf6865cedf5e5cb6ef27c47a0dedeb37f
Alexsaphir <alex.game@sfr.fr>
2018-03-31 19:04:05 +0200

    Add copy constructor and const &

--- Source/vectorr2.cpp
+++ Source/vectorr2.cpp
@@ -12,22 +12,44 @@ VectorR2::VectorR2(double a, double b)
 	y = b;
 }
 
-VectorR2 operator+(VectorR2 A, VectorR2 B)
+VectorR2::VectorR2(const VectorR2 &A)
+{
+	x = A.x;
+	y = A.y;
+}
+
+void VectorR2::abs()
+{
+	x = std::abs(x);
+	y = std::abs(y);
+}
+
+VectorR2 operator+(const VectorR2 &A, const VectorR2 &B)
 {
 	return VectorR2(A.x + B.x, A.y + B.y);
 }
 
-VectorR2 operator-(VectorR2 A, VectorR2 B)
+VectorR2 operator-(const VectorR2 &A, const VectorR2 &B)
 {
 	return VectorR2(A.x - B.x, A.y - B.y);
 }
 
-VectorR2 operator*(double d, VectorR2 B)
+VectorR2 operator*(double d, const VectorR2 &B)
 {
 	return VectorR2(d*B.x, d * B.y);
 }
 
-VectorR2 operator*(VectorR2 A, double d)
+VectorR2 operator*(const VectorR2 &A, double d)
 {
 	return d*A;
+}
+
+VectorR2 operator/(const VectorR2 &A, double d)
+{
+	return VectorR2(A.x/d, A.y/d);
+}
+
+VectorR2 abs(const VectorR2 &A)
+{
+	return VectorR2(A).abs();
 }
--- Source/vectorr2.h
+++ Source/vectorr2.h
@@ -1,20 +1,27 @@
 #ifndef VECTORR2_H
 #define VECTORR2_H
 
+#include <cmath>
+
 class VectorR2
 {
 public:
 	VectorR2();
 	VectorR2(double a, double b);
+	VectorR2(const VectorR2 &A);
+
+	void abs();
 
 	double x;
 	double y;
 };
 
-VectorR2 operator+(VectorR2 A, VectorR2 B);
-VectorR2 operator-(VectorR2 A, VectorR2 B);
-VectorR2 operator*(double d, VectorR2 B);
-VectorR2 operator*(VectorR2 A, double d);
+VectorR2 operator+(const VectorR2 &A, const VectorR2 &B);
+VectorR2 operator-(const VectorR2 &A, const VectorR2 &B);
+VectorR2 operator*(double d, const VectorR2 &B);
+VectorR2 operator*(const VectorR2 &A, double d);
+VectorR2 operator/(const VectorR2 &A, double d);
+VectorR2 abs(const VectorR2 &A);
 
 
 #endif // VECTORR2_H
