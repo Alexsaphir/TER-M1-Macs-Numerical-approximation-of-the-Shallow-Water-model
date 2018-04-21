49e15c7579d3cee96d76dfe80832b1947f2b7898
Alexsaphir <alex.game@sfr.fr>
2018-04-21 22:18:04 +0200

    time code

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,19 +1,31 @@
 #include <QCoreApplication>
 
+#include <chrono>
+
 #include "solvercoupledlfsv.h"
+#include "solvercoupledlf0.h"
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
 
-	//SolverCoupledLF0 S(5., 3.);
-	//S.solve();
-
-	SolverCoupledLFSV S(1., 1.);
-	S.initialCondition();
-
+	SolverCoupledLF0 S(3., 1.);
 	S.solve();
-	S.exportData();
+
+	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+	//SolverCoupledLFSV S(3., 1.);
+	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+	std::cout << "Init duration (ms): " << duration << std ::endl;
+
+
+	t1 = std::chrono::high_resolution_clock::now();
+	//S.solve();
+	t2 = std::chrono::high_resolution_clock::now();
+	duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+	std::cout << "Solving duration (ms): " << duration << std ::endl;
+
+
 
 	a.quit();
 }
