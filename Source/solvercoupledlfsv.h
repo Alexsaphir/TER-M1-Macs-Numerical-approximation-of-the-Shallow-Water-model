43781456a06bb51bcf1f303593c1ae558be83073
Alexsaphir <alex.game@sfr.fr>
2018-04-21 17:36:43 +0200

    Reset work progress

--- Source/solvercoupledlfsv.h
+++ Source/solvercoupledlfsv.h
@@ -12,30 +12,12 @@ class SolverCoupledLFSV : public Solver
 {
 public:
 	SolverCoupledLFSV(double l, double r);
+
 	void initialCondition();
 	void solve();
-	void exportData() const;
 
 private:
-	VectorR2 F(const VectorR2 &W) const;
-	double F1(const VectorR2 &W) const;
-	double F2(const VectorR2 &W) const;
-	void evaluateFlux();
 
-	double getCorrected_Z_half(int i) const;
-
-	double getCorrected_H_half_pos(int i) const;
-	double getCorrected_H_half_min(int i) const;
-
-	VectorR2 S(int i) const;
-
-	VectorR2 getCorrected_U_half_pos(int i) const;
-	VectorR2 getCorrected_U_half_min(int i) const;
-
-
-	void computeNext();
-	double computeCFL() const;
-	void swapCoupledGrid();
 
 private:
 	double m_uL;
