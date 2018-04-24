95bd20d5e2f9a874a37fcde1e6264031201e7c21
Alexsaphir <alex.game@sfr.fr>
2018-04-24 10:56:24 +0200

    Doxygen

--- Source/grid.cpp
+++ Source/grid.cpp
@@ -1,22 +1,38 @@
 #include "grid.h"
 
+///
+/// \brief Grid::Grid
+/// \param N Number of element in the grid
+///
 Grid::Grid(int N): m_n(N)
 {
 	m_V.reserve(m_n);
 	m_V.fill(0, m_n);
 }
 
+///
+/// \brief Grid::~Grid
+///
 Grid::~Grid()
 {
 	m_V.clear();
 	m_V.squeeze();
 }
 
+///
+/// \brief Grid::size
+/// \return Number of elements in the grid.
+///
 int Grid::size() const
 {
 	return m_n;
 }
 
+///
+/// \brief Grid::get
+/// \param i Position on the grid
+/// \return Value of the grid at the position i.
+///
 double Grid::get(int i) const
 {
 	if(i<0 || i>=m_n)
@@ -27,6 +43,11 @@ double Grid::get(int i) const
 	return m_V.at(i);
 }
 
+///
+/// \brief Grid::set
+/// \param i Position on the grid
+/// \param v Value to set
+///
 void Grid::set(int i, double v)
 {
 	if(i<0 || i>=m_n)
--- Source/grid.h
+++ Source/grid.h
@@ -5,6 +5,9 @@
 
 #include <QDebug>
 
+///
+/// \brief The Grid class
+/// describe a 1D-Grid who store value of N elements.
 class Grid
 {
 public:
@@ -19,7 +22,13 @@ public:
 	bool isValidIndex(int i) const;
 
 protected:
+	///
+	/// \brief m_n
+	/// Number of element in the grid.
 	int m_n;
+	///
+	/// \brief m_V
+	/// Store value of the elements in the grid.
 	QVector<double> m_V;
 };
 #endif // GRID_H
