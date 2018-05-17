693d411f3905dfa0b1a7e8450f89ade8cd25c59e
Alexsaphir <alex.game@sfr.fr>
2018-05-17 11:12:20 +0200

    Change timing code

--- Source/main.cpp
+++ Source/main.cpp
@@ -12,22 +12,15 @@ int main(int argc, char *argv[])
 	//SolverCoupledLF0 S(3., 1.);
 	//S.solve();
 
-	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
-
 	SolverCoupledLFSV S;
 
-	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-	std::cout << "Init duration (ms): " << duration << std ::endl;
-
-
-	t1 = std::chrono::high_resolution_clock::now();
+	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
 	S.solve();
 
-	t2 = std::chrono::high_resolution_clock::now();
-	duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
-	std::cout << "Solving duration (ms): " << duration << std ::endl;
+	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::duration<double>>( t2 - t1 ).count();
+	std::cout << "Solving duration (s): " << duration << std ::endl;
 
 	a.quit();
 }
