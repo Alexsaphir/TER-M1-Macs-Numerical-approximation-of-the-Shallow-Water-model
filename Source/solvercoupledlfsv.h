42b75c3f88908e323ae03f792cf4b2f6d3616694
Alexsaphir <alex.game@sfr.fr>
2018-05-17 11:14:32 +0200

    Set private/protected

--- Source/solvercoupledlfsv.h
+++ Source/solvercoupledlfsv.h
@@ -16,38 +16,44 @@ public:
 	void initialCondition();
 	void solve();
 
+protected:
+
+	double getZ(int i) const;
+
+
+	double getH(int i) const;
+	VectorR2 getU(int i) const;
+
 private:
+	double getZ_mh(int i) const;
+	double getZ_ph(int i) const;
 
-	inline double getZ(int i) const;
-	inline double getZ_mh(int i) const;
-	inline double getZ_ph(int i) const;
+	double getH_mhm(int i) const;
+	double getH_mhp(int i) const;
+	double getH_phm(int i) const;
+	double getH_php(int i) const;
 
-	inline double getH(int i) const;
-	inline double getH_mhm(int i) const;
-	inline double getH_mhp(int i) const;
-	inline double getH_phm(int i) const;
-	inline double getH_php(int i) const;
 
-	inline VectorR2 getU(int i) const;
-	inline VectorR2 getU_mhm(int i) const;
-	inline VectorR2 getU_mhp(int i) const;
-	inline VectorR2 getU_phm(int i) const;
-	inline VectorR2 getU_php(int i) const;
+	VectorR2 getU_mhm(int i) const;
+	VectorR2 getU_mhp(int i) const;
+	VectorR2 getU_phm(int i) const;
+	VectorR2 getU_php(int i) const;
 
-	inline VectorR2 getS(int i) const;
+	VectorR2 getS(int i) const;
 
-	inline VectorR2 getFlux_mh(int i) const;
-	inline VectorR2 getFlux_ph(int i) const;
+protected:
+	VectorR2 getFlux_mh(int i) const;
+	VectorR2 getFlux_ph(int i) const;
 	void evaluateFlux();
 
-	inline VectorR2 F(VectorR2 w) const;
-	inline double F1(VectorR2 w) const;
-	inline double F2(VectorR2 w) const;
-	inline VectorR2 Flux(VectorR2 wL, VectorR2 wR) const;
+	VectorR2 F(VectorR2 w) const;
+	double F1(VectorR2 w) const;
+	double F2(VectorR2 w) const;
+	VectorR2 Flux(VectorR2 wL, VectorR2 wR) const;
 
 	void computeNext();
 	double computeCFL() const;
-	inline void swapCoupledGrid();
+	void swapCoupledGrid();
 
 	void computeSpeed();
 private:
